488680e9ffa17ce828f2f53f5712c634c305947a
alberto1996-23 <albertowilliams16@gmail.com>
2026-03-08 20:57:06 -0500

    Add test for PetClinicRuntimeHints to improve coverage

--- src/test/java/org/springframework/samples/petclinic/PetClinicRuntimeHintsTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicRuntimeHintsTests.java
@@ -5,26 +5,11 @@ import org.springframework.aot.hint.RuntimeHints;
 
 class PetClinicRuntimeHintsTests {
 
-    @Test
-    void registerHintsDoesNotThrow() {
-        PetClinicRuntimeHints registrar = new PetClinicRuntimeHints();
-        RuntimeHints hints = new RuntimeHints();
-        registrar.registerHints(hints, getClass().getClassLoader());
-    }
-
-}
-package org.springframework.samples.petclinic;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.aot.hint.RuntimeHints;
-
-class PetClinicRuntimeHintsTests {
-
-    @Test
-    void registerHintsDoesNotThrow() {
-        PetClinicRuntimeHints registrar = new PetClinicRuntimeHints();
-        RuntimeHints hints = new RuntimeHints();
-        registrar.registerHints(hints, getClass().getClassLoader());
-    }
+	@Test
+	void registerHintsDoesNotThrow() {
+		PetClinicRuntimeHints registrar = new PetClinicRuntimeHints();
+		RuntimeHints hints = new RuntimeHints();
+		registrar.registerHints(hints, getClass().getClassLoader());
+	}
 
 }
